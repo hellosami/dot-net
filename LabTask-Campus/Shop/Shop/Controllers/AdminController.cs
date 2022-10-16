bd452bb9019e0558bceab0bf5ca59a5a0ddf76a2
Adnan Sami <sami.cse1@gmail.com>
2022-10-17 01:33:11 +0600

    LabTask Campus

---
+++ LabTask-Campus/Shop/Shop/Controllers/AdminController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Shop.DB;
+using Newtonsoft.Json;
+
+namespace Shop.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var db = new ShopDbEntities();
+
+            var productList = db.Products.ToList();
+
+            return View(productList);
+           
+        }
+
+        [HttpGet]
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddProduct(Product p)
+        {
+            var db = new ShopDbEntities();
+
+            //var p = new Product();
+            p.Name = p.Name;
+            p.Price = p.Price;
+            p.Qty = p.Qty;
+            db.Products.Add(p);
+
+            db.SaveChanges();
+            TempData["MSG_ADMIN"] = "PRODUCT HAS BEED ADDED SUCCESSFULY!";
+            return RedirectToAction("AddProduct");
+        }
+
+        
+   
+        public ActionResult DeleteProduct(int id)
+        {
+            var db = new ShopDbEntities();
+
+            //var p = new Product();
+            var a = db.Products.Where(i => i.Id == id).SingleOrDefault();
+            db.Products.Remove(a);
+
+            db.SaveChanges();
+            TempData["MSG_ADMIN"] = "PRODUCT HAS BEED DELETED SUCCESSFULY!";
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public ActionResult EditProduct(int id)
+        {
+            var db = new ShopDbEntities();
+
+            //var p = new Product();
+            var a = db.Products.Where(i => i.Id == id).SingleOrDefault();
+  
+            
+       
+            return View(a);
+        }
+
+
+        [HttpPost]
+        public ActionResult EditProduct(Product p)
+        {
+            var db = new ShopDbEntities();
+
+            //var p = new Product();
+            var a = db.Products.Where(i => i.Id == p.Id).SingleOrDefault();
+            a.Name = p.Name;
+            a.Price = p.Price;
+            a.Qty = p.Qty;
+
+            db.SaveChanges();
+            TempData["MSG_ADMIN"] = "PRODUCT HAS BEED UPDATED SUCCESSFULY!";
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ LabTask-Campus/Shop/Shop/Controllers/ShopController.cs
@@ -0,0 +1,121 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Shop.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace Shop.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Products_()
+        {
+            var db = new ShopDbEntities();
+
+            var productList = db.Products.ToList();
+
+            if (Session["Cart"] != null)
+            {
+                string json = Session["Cart"].ToString();
+                List<Product> products = JsonConvert.DeserializeObject<List<Product>>(json);
+                ViewBag.Cart = products;
+            }
+
+            return View(productList);
+         
+        }
+
+        public ActionResult AddToCart(int id)
+        {
+
+            var JSON = "";
+            List<Product> products = new List<Product>();
+            Product product = new Product();
+
+            var db = new ShopDbEntities();
+            var p = db.Products.Where(i => i.Id == id).SingleOrDefault();
+
+            if (Session["Cart"] == null)
+            {
+                product.Id = p.Id;
+                product.Name = p.Name;
+                product.Price = p.Price;
+                product.Qty = p.Qty;
+                products.Add(product);
+                JSON = new JavaScriptSerializer().Serialize(products);
+                Session["Cart"] = new JavaScriptSerializer().Serialize(products);
+
+                products.Clear();
+            }
+            else
+            {
+                //string json = "{\"Id\":0,\"Name\":\"M10\",\"Price\":\"1000\",\"Qty\":\"20\"}";
+                //products.Add(new JavaScriptSerializer().Deserialize<Product>(json));
+
+                string json = Session["Cart"].ToString();
+                products = JsonConvert.DeserializeObject<List<Product>>(json);
+
+                product.Id = p.Id;
+                product.Name = p.Name;
+                product.Price = p.Price;
+                product.Qty = p.Qty;
+                products.Add(product);
+
+                JSON = new JavaScriptSerializer().Serialize(products);
+                Session["Cart"] = JSON;
+                products.Clear();
+            }
+            return RedirectToAction("Products_");
+        }
+
+        public ActionResult Checkout()
+        {
+            if (Session["Cart"] != null)
+            {
+                string json = Session["Cart"].ToString();
+                List<Product> products = JsonConvert.DeserializeObject<List<Product>>(json);
+                ViewBag.Cart = products;
+
+            }
+
+
+            return View();
+        }
+
+        public ActionResult AddOrder()
+        {
+            var db = new ShopDbEntities();
+
+            string json = Session["Cart"].ToString();
+            var order = new Order();
+            order.Orders = json;
+
+            db.Orders.Add(order);
+
+            db.SaveChanges();
+
+            Session["Cart"] = null;
+            TempData["MSG"] = "YOUR ORDER HAS BEEN CONFIRMED!";
+            return RedirectToAction("Products_");
+        }
+
+        public ActionResult ClearCart()
+        {
+            Session["Cart"] = null;
+            return RedirectToAction("Products_");
+        }
+
+       
+    }
+}
